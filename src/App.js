9058aedfd2deec9978c697131f76896a2872e6f0
Lalit Sharma <lalit9013@gmail.com>
2022-06-02 21:30:16 +0530

    Color Palette Generator

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#f15025").all(10));
+  const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  const getRandomHex = () => {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[Math.floor(Math.random() * hex.length)];
+    }
+    return hexColor;
+  };
+
+  const randomizeHandler = () => {
+    let random = getRandomHex();
+    let colors = new Values(random).all(10);
+    setList(colors);
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="Enter Color"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+          <button className="btn randomize" onClick={randomizeHandler}>
+            Randomize
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
